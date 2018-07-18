721debcb0e7b1e23d734399352fd6381d58ab3df
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-07-18 14:26:52 -0400

    SVG Images on Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import IconHome from './IconHome';
+import IconAircraft from './IconAircraft';
+import IconKey from './IconKey';
+import IconMap from './IconMap';
 
 class Sidebar extends Component {
   render() {
     return (
-      <div className="sidebar">
-          Sidebar
-      </div>
+        <div className="sidebar">
+          <ul className="side-nav">
+            <li className = "side-nav__item"> 
+              <a href="#" className="side-nav__link">
+                <IconHome/>
+                <span>Hotel</span>
+              </a>
+            </li>
+            <li className = "side-nav__item"> 
+              <a href="#" className="side-nav__link">
+                <IconAircraft/>
+                <span>Hotel</span>
+              </a>
+            </li>
+            <li className = "side-nav__item"> 
+              <a href="#" className="side-nav__link">
+                <IconKey/>
+                <span>Hotel</span>
+              </a>
+            </li>
+            <li className = "side-nav__item"> 
+              <a href="#" className="side-nav__link">
+                <IconMap/>
+                <span>Hotel</span>
+              </a>
+            </li>
+          </ul>
+        </div>
     );
   }
 }
