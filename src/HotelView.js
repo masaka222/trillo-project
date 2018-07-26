d13f0ab243c6e9b9e6721722ecbde0111c3621a6
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-07-25 20:44:50 -0400

    Hotel View Flex Images

--- src/HotelView.js
+++ src/HotelView.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import hotel1 from "./hotel-1.jpg";
+import hotel2 from "./hotel-2.jpg";
+import hotel3 from "./hotel-3.jpg";
 
 class HotelView extends Component {
   render() {
     return (
       <div className="hotel-view">
         <main>
-          HotelView
+          <div className="gallery">
+            <figure className="gallery__item">
+              <img className="gallery__photo" src = {hotel1}/>
+            </figure>
+            <figure className="gallery__item">
+              <img className="gallery__photo" src = {hotel2}/>
+            </figure>
+            <figure className="gallery__item">
+              <img className="gallery__photo" src = {hotel3}/>
+            </figure>
+          </div>
         </main>
       </div>
     );
