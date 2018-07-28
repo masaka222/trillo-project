ba9b2fa4a13333ba86c6fd6294a436e48be3e92d
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-07-27 23:58:00 -0400

    Hotel View Flexbox Implementation

--- src/HotelView.js
+++ src/HotelView.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import hotel1 from "./hotel-1.jpg";
 import hotel2 from "./hotel-2.jpg";
 import hotel3 from "./hotel-3.jpg";
+import IconStar from "./IconStar";
+import IconLocation from "./IconLocation";
 
 class HotelView extends Component {
   render() {
@@ -18,6 +20,34 @@ class HotelView extends Component {
             <figure className="gallery__item">
               <img className="gallery__photo" src = {hotel3}/>
             </figure>
+          </div>
+
+          <div className="overview">
+            <h1 className="overview__heading">
+              Hotel Las Palmas
+            </h1>
+            
+            <div className="overview__stars">
+              <IconStar width="1.75rem" height="1.75rem" fill="#eb2f64"/>
+              <IconStar width="1.75rem" height="1.75rem" fill="#eb2f64"/>
+              <IconStar width="1.75rem" height="1.75rem" fill="#eb2f64"/>
+              <IconStar width="1.75rem" height="1.75rem" fill="#eb2f64"/>
+              <IconStar width="1.75rem" height="1.75rem" fill="#eb2f64"/>
+            </div>
+
+            <div className="overview__location">
+              <IconLocation width="1.75rem" height="1.75rem" fill="#eb2f64"/>
+              <button className="btn-inline">Albufeira, Portugal</button>
+            </div>
+
+            <div className="overview__rating">
+              <div className="overview__rating-average">
+                8.6
+              </div>
+              <div className="overview__rating-count">
+                429 votes
+              </div>
+            </div>
           </div>
         </main>
       </div>
